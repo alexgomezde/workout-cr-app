a0bf72857977722953073e7c594de83121360589
alexgomezde <alexgomezde@gmail.com>
2024-06-02 12:07:11 -0600

    Update deploy settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/workout-cr-app/",
+  base: "https://alexgomezde.github.io/workout-cr-app",
   plugins: [react()],
 })
