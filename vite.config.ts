5bdfdfc099ab004687877c74a15859f422a9c29a
alexgomezde <alexgomezde@gmail.com>
2024-06-02 12:52:50 -0600

    Update base order

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://alexgomezde.github.io/workout-cr-app",
   plugins: [react()],
+  base: "https://alexgomezde.github.io/workout-cr-app",
 })
